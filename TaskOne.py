1ace6382b41496265a01258f9ae7ecfc9646a3c3
Mohsin <bsef19m031@pucit.edu.pk>
2022-03-23 20:26:05 +0500

    Fetch from from Github failed and now doing nothing

--- TaskOne.py
+++ TaskOne.py
@@ -6,6 +6,4 @@ print("This is fun.")
 print("Above all is try one.")
 
 
-# I have written this comment using GitHub online editing option.
-# Now I have to fetch it into my offline VSCode.
-# Let me google how to do that and I'll come back soon.
+
