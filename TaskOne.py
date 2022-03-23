272d0520f7797d4ab2f1bead2acedd1a9b88b29d
Mohsin <57590526+aumohsin99@users.noreply.github.com>
2022-03-23 20:18:23 +0500

    Add some comments using online editing of my file.
    Added comments

--- TaskOne.py
+++ TaskOne.py
@@ -4,3 +4,8 @@ print("I like typing this.")
 print("This is fun.")
 
 print("Above all is try one.")
+
+
+# I have written this comment using GitHub online editing option.
+# Now I have to fetch it into my offline VSCode.
+# Let me google how to do that and I'll come back soon.
