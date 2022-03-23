7c0e94932e8704b741713496b979cd1699d81b64
Mohsin <bsef19m031@pucit.edu.pk>
2022-03-23 20:32:35 +0500

    Final try before fetching data

--- TaskOne.py
+++ TaskOne.py
@@ -7,3 +7,5 @@ print("Above all is try one.")
 
 # I am stuck here on VSCode and don't know what to do
 
+# Ok. Why to give up? Let's try again.
+# Committing the above comment but so far failed in fetching the online document.
